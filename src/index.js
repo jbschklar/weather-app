024e0420c0fedf76567d294ee54fd325c63d01c6
jbschklar <jbschklar@gmail.com>
2022-10-06 17:01:54 -0400

    added functions to display innerHTML for current location day forcast and styled display

--- src/index.js
+++ src/index.js
@@ -1,15 +1,49 @@
 import "./style.css";
+import { lightFormat } from "date-fns";
+
+const sunIcon = "/sun.png";
+
+const oneDayDisplay = function (data) {
+	const display = document.querySelector(".display-container");
+	display.innerHTML = `<h1>${data.name}</h1>
+    <div class="temp-container">
+        <div class="temp-data">
+            <h2 class="date">${lightFormat(new Date(), "MM/dd/yyyy")}</h2>
+            <span class="high-low">High ${Math.round(
+							data.main.temp_max
+						)}° Low ${Math.round(data.main.temp_min)}°</span>
+            <span class="curr-temp">${Math.round(data.main.temp)}°F</span>
+            <span class="feels-like">Feels like ${Math.round(
+							data.main.feels_like
+						)}°</span>
+        </div>
+        <div class="icon-container">
+			<img src=${sunIcon} class="temp-icon" />
+			<span class="icon-description">${data.weather[0].main}</span>
+		</div>
+    </div>`;
+};
 
 const getLocalWeather = async function (position) {
 	try {
 		const { latitude } = position.coords;
 		const { longitude } = position.coords;
+		const today = "weather";
+		const fiveDay = "forcast";
 		const response = await fetch(
-			`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=e548290df6fc88b40ad44f22ee99dc3f&units=imperial`,
+			`https://api.openweathermap.org/data/2.5/${today}?lat=${latitude}&lon=${longitude}&appid=e548290df6fc88b40ad44f22ee99dc3f&units=imperial`,
 			{ mode: "cors" }
 		);
 		const data = await response.json();
-		console.log(data.name, data.main.temp);
+		console.log(
+			data.name,
+			data.main.temp,
+			data.weather[0].main,
+			data.weather[0].description,
+			data.main.temp_min,
+			data.main.temp_max
+		);
+		oneDayDisplay(data);
 	} catch (error) {
 		console.log(error);
 	}
