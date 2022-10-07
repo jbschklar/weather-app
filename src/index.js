e0fde97143d461a6a594234e34460370cca77586
jbschklar <jbschklar@gmail.com>
2022-10-07 15:51:57 -0400

    differentiated weather API call based on tab clicked to today or five day formatted display

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import "./style.css";
 import { lightFormat } from "date-fns";
 
 const sunIcon = "/sun.png";
+const tabs = document.querySelectorAll(".tab");
+const display = document.querySelector(".container");
 
 const oneDayDisplay = function (data) {
-	const display = document.querySelector(".display-container");
-	display.innerHTML = `<h1>${data.name}</h1>
+	display.classList.remove("five-day");
+	display.innerHTML = `
+    <h1>${data.name}</h1>
     <div class="temp-container">
         <div class="temp-data">
             <h2 class="date">${lightFormat(new Date(), "MM/dd/yyyy")}</h2>
@@ -24,34 +27,106 @@ const oneDayDisplay = function (data) {
     </div>`;
 };
 
-const getLocalWeather = async function (position) {
+const fiveDayDisplay = function (data) {
+	display.classList.add("five-day");
+	display.innerHTML = `
+    <h1>${data.city.name}</h1>
+				<div class="forcast day-one">
+					<h2 class="forcast-date">${formatDate(data)}</h2>
+					<div class="highlow-container">
+						<span class="high-temp">High: ${Math.round(data.list[0].main.temp_max)}°</span>
+						<span class="low-temp">Low: ${Math.round(data.list[0].main.temp_min)}°</span>
+					</div>
+					<div class="icon-container">
+						<img src="/sun.png" class="forcast-icon" />
+					</div>
+				</div>
+				<div class="forcast day-two">
+					<h2 class="forcast-date">Tuesday</h2>
+					<div class="highlow-container">
+						<span class="high-temp">High: 75°</span>
+						<span class="low-temp">Low: 50°</span>
+					</div>
+					<div class="icon-container">
+						<img src="/sun.png" class="forcast-icon" />
+					</div>
+				</div>
+				<div class="forcast day-three">
+					<h2 class="forcast-date">Wednesday</h2>
+					<div class="highlow-container">
+						<span class="high-temp">High: 75°</span>
+						<span class="low-temp">Low: 50°</span>
+					</div>
+					<div class="icon-container">
+						<img src="/sun.png" class="forcast-icon" />
+					</div>
+				</div>
+				<div class="forcast day-four">
+					<h2 class="forcast-date">Thursday</h2>
+					<div class="highlow-container">
+						<span class="high-temp">High: 75°</span>
+						<span class="low-temp">Low: 50°</span>
+					</div>
+					<div class="icon-container">
+						<img src="/sun.png" class="forcast-icon" />
+					</div>
+				</div>
+				<div class="forcast day-five">
+					<h2 class="forcast-date">Friday</h2>
+					<div class="highlow-container">
+						<span class="high-temp">High: 75°</span>
+						<span class="low-temp">Low: 50°</span>
+					</div>
+					<div class="icon-container">
+						<img src="/sun.png" class="forcast-icon" />
+					</div>
+				</div>
+    `;
+};
+
+const formatDate = function (data) {
+	const date = data.list[0].dt_txt.slice(0, 10);
+	const year = date.slice(0, 4);
+	const month = date.slice(5, 7);
+	const day = date.slice(8, 10);
+	return `${month}/${day}/${year}`;
+};
+const getLocalWeather = async function (position, type) {
 	try {
 		const { latitude } = position.coords;
 		const { longitude } = position.coords;
 		const today = "weather";
-		const fiveDay = "forcast";
+		const fiveDay = "forecast";
 		const response = await fetch(
-			`https://api.openweathermap.org/data/2.5/${today}?lat=${latitude}&lon=${longitude}&appid=e548290df6fc88b40ad44f22ee99dc3f&units=imperial`,
+			`https://api.openweathermap.org/data/2.5/${type}?lat=${latitude}&lon=${longitude}&appid=e548290df6fc88b40ad44f22ee99dc3f&units=imperial`,
 			{ mode: "cors" }
 		);
 		const data = await response.json();
-		console.log(
-			data.name,
-			data.main.temp,
-			data.weather[0].main,
-			data.weather[0].description,
-			data.main.temp_min,
-			data.main.temp_max
-		);
-		oneDayDisplay(data);
+
+		type === "weather" ? oneDayDisplay(data) : fiveDayDisplay(data);
 	} catch (error) {
 		console.log(error);
 	}
 };
-const init = function () {
-	navigator.geolocation.getCurrentPosition(getLocalWeather, (err) => {
-		console.log(err);
-	});
+const init = function (type) {
+	navigator.geolocation.getCurrentPosition(
+		(pos) => getLocalWeather(pos, type),
+		(err) => {
+			console.log(err);
+		}
+	);
 };
 
-init();
+tabs.forEach((tab) => {
+	tab.addEventListener("click", (e) => {
+		tabs.forEach((tab) => tab.classList.toggle("selected"));
+		display.innerHTML = "";
+		if (e.target.textContent === "Today") {
+			init("weather");
+		} else {
+			init("forecast");
+		}
+	});
+});
+
+// init();
