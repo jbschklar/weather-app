7dfa735579062f67bdfa688c3b3c8186b1a2480d
jbschklar <jbschklar@gmail.com>
2022-10-12 12:26:41 -0400

    refactored API fetch functions and handler to handle local weather on page load and user search data.

--- src/index.js
+++ src/index.js
@@ -13,29 +13,30 @@ const search = document.getElementById("location-search");
 const display = document.querySelector(".container");
 let location;
 
+// populates display for current weather under Today tab
 const oneDayDisplay = function (data) {
-	const today = getDays(data.list)[0];
 	display.classList.remove("five-day");
 	display.innerHTML = `
-    <h1>${data.city.name}</h1>
+    <h1>${data.name}</h1>
     <div class="temp-container">
         <div class="temp-data">
             <div>
-            <h2 class="date">${formatDate(today[0])}</h2>
+            <h2 class="date">${lightFormat(new Date(), "MM/dd/yyyy")}</h2>
             <h3>Current Temp</h3>
-            <span class="curr-temp">${Math.round(today[0].main.temp)}°F</span>
+            <span class="curr-temp">${Math.round(data.main.temp)}°F</span>
             </div>
             <span class="feels-like">Feels like ${Math.round(
-							today[0].main.feels_like
+							data.main.feels_like
 						)}°</span>
         </div>
         <div class="icon-container">
-			<img src=${getIcon(today[0].weather[0].main)} class="temp-icon" />
-			<span class="icon-description">${today[0].weather[0].main}</span>
+			<img src=${getIcon(data.weather[0].main)} class="temp-icon" />
+			<span class="icon-description">${data.weather[0].main}</span>
 		</div>
     </div>`;
 };
 
+// returns array to seperate forcast API call list array which is in 3 hr increments into days
 const getDays = function (arr) {
 	const today = arr[0].dt_txt.slice(8, 10);
 	console.log(today);
@@ -60,6 +61,7 @@ const getIcon = function (str) {
 	return sunIcon;
 };
 
+// returns high/low temp for each day since free version of open weather API doesn't have this feature
 const getHighLow = function (arr) {
 	let high = arr[0].main.temp_max;
 	let low = arr[0].main.temp_min;
@@ -71,13 +73,14 @@ const getHighLow = function (arr) {
 	return { high, low };
 };
 
+// populates 5 day display tab
 const fiveDayDisplay = function (data) {
 	console.log(data);
-	const forcastArr = getDays(data.list).slice(1);
+	const forcastArr = getDays(data.list);
 	console.log(forcastArr);
 	display.classList.add("five-day");
 	display.innerHTML = `<h1>${data.city.name}</h1>`;
-	for (let i = 0; i < 5; i++) {
+	for (let i = 1; i <= 5; i++) {
 		const forcast = document.createElement("div");
 		const { high, low } = getHighLow(forcastArr[i]);
 
@@ -104,61 +107,74 @@ const formatDate = function (data) {
 	const day = date.slice(8, 10);
 	return `${month}/${day}/${year}`;
 };
-const getLocalWeather = async function (position) {
+
+// this is to determin which API call to use, weather for current and forcast for 5 day calls
+const getCallType = function () {
 	const selected = document.querySelector(".selected");
+	const type = selected.textContent === "Today" ? "weather" : "forecast";
+	return type;
+};
+
+// this populates display with API call based on user's current location data on page load
+const getLocalWeather = async function (position) {
 	try {
 		const { latitude } = position.coords;
 		const { longitude } = position.coords;
+
 		const response = await fetch(
-			`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=e548290df6fc88b40ad44f22ee99dc3f&units=imperial`,
+			`https://api.openweathermap.org/data/2.5/${getCallType()}?lat=${latitude}&lon=${longitude}&appid=e548290df6fc88b40ad44f22ee99dc3f&units=imperial`,
 			{ mode: "cors" }
 		);
 		const data = await response.json();
+		console.log(data);
 
-		selected.textContent === "Today"
-			? oneDayDisplay(data)
-			: fiveDayDisplay(data);
+		getCallType() === "weather" ? oneDayDisplay(data) : fiveDayDisplay(data);
 	} catch (error) {
 		console.log(error);
 	}
 	display.classList.remove("loading");
 };
 
-const getUSWeather = async function (location, type, state) {
+// API call from user search data
+const getWeather = async function (location) {
+	let city;
+	let state;
+
+	[city, state] = location.split(",");
+	console.log(city, state);
 	try {
-		const response = await fetch(
-			`https://api.openweathermap.org/data/2.5/${type}?q=${location},${state},US&appid=e548290df6fc88b40ad44f22ee99dc3f&units=imperial`,
-			{ mode: "cors" }
-		);
+		const response = state
+			? await fetch(
+					`https://api.openweathermap.org/data/2.5/${getCallType()}?q=${city},${state},US&appid=e548290df6fc88b40ad44f22ee99dc3f&units=imperial`,
+					{ mode: "cors" }
+			  )
+			: await fetch(
+					`https://api.openweathermap.org/data/2.5/${getCallType()}?q=${city}&appid=e548290df6fc88b40ad44f22ee99dc3f&units=imperial`,
+					{ mode: "cors" }
+			  );
 		const data = await response.json();
 		console.log(data);
+		getCallType() === "weather" ? oneDayDisplay(data) : fiveDayDisplay(data);
 	} catch (error) {
 		console.log(error);
 	}
+	display.classList.remove("loading");
 };
 
-const getIntWeather = async function (location, type, country) {
-	try {
-		const response = await fetch(
-			`https://api.openweathermap.org/data/2.5/${type}?q=${location},${country}&appid=e548290df6fc88b40ad44f22ee99dc3f&units=imperial`,
-			{ mode: "cors" }
-		);
-		const data = await response.json();
-		console.log(data);
-	} catch (error) {
-		console.log(error);
-	}
-};
-
-const init = function () {
+// handles API calls either on page load or user search selection and pauses UI while loading to prevent errors
+const weatherHandler = function (type) {
 	display.classList.add("loading");
 	display.innerHTML = "<p>Loading ...</p>";
-	navigator.geolocation.getCurrentPosition(
-		(pos) => getLocalWeather(pos),
-		(err) => {
-			console.log(err);
-		}
-	);
+	if (location) {
+		getWeather(location);
+	} else {
+		navigator.geolocation.getCurrentPosition(
+			(pos) => getLocalWeather(pos, type),
+			(err) => {
+				console.log(err);
+			}
+		);
+	}
 };
 
 tabs.forEach((tab) => {
@@ -170,24 +186,20 @@ tabs.forEach((tab) => {
 			return;
 		tabs.forEach((tab) => tab.classList.toggle("selected"));
 		display.innerHTML = "";
-		if (e.target.textContent === "Today") {
-			init("forecast");
-		} else {
-			init("forecast");
-		}
+		weatherHandler();
 	});
 });
 
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
-	console.log(search.value);
+	location = search.value;
+	weatherHandler();
 });
 
-init("forecast");
-getUSWeather("chicago", "weather", "IL");
-getIntWeather("london", "weather");
+// fn call on page load
+weatherHandler();
 
 //////////// Check List
 // - add loading icon/display img
-// - add search function
+
 // - tweek input to only allow city, zip, or city & state
